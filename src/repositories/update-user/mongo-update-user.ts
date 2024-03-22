215417b4f8f24ad9fdebd34baed94c27aaf6c668
Xannd <alexandreocosta2812@gmail.com>
2024-03-22 17:42:48 -0300

    refactor: add MongoUser

--- src/repositories/update-user/mongo-update-user.ts
+++ src/repositories/update-user/mongo-update-user.ts
@@ -2,6 +2,7 @@ import { ObjectId } from "mongodb";
 import { IUpdateUserRepository, UpdateUserParams } from "../../controllers/update-user/protocols";
 import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
+import { MongoUser } from "../mongo-protocols";
 
 export class MongoUpdateUserRepository implements IUpdateUserRepository {
     async updateUser(id: string, params: UpdateUserParams): Promise<User> {
@@ -14,7 +15,7 @@ export class MongoUpdateUserRepository implements IUpdateUserRepository {
             }
         );
 
-        const user = await MongoClient.db.collection<Omit<User, "id">>('users').findOne({_id: new ObjectId(id)})
+        const user = await MongoClient.db.collection<MongoUser>('users').findOne({_id: new ObjectId(id)})
 
         if(!user){
             throw new Error("User not updated");
